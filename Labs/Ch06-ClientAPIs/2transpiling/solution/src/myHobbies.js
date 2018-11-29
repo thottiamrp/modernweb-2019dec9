be56224be6317ec8a75f151757bbb4fc3b206680
Judy Lipinski <judy@karmoxie.com>
2018-11-29 10:15:41 -0500

    updated solution for loop

--- Labs/Ch06-ClientAPIs/2transpiling/solution/src/myHobbies.js
+++ Labs/Ch06-ClientAPIs/2transpiling/solution/src/myHobbies.js
@@ -8,9 +8,19 @@ function printHobbyInfo(hobby) {
     console.log(` ${hobby.name} has been an interest for ${hobby.lengthInYearsAtHobby} years`)
 }
 
-for (let hobbyIndex = 0; index < hobbiesArray.length; index++) {
+for (let hobbyIndex = 0; hobbyIndex < hobbiesArray.length; hobbyIndex++) {
     const hobby = hobbiesArray[hobbyIndex];
     printHobbyInfo(hobby);
+}
+
+function returnHobbiesHTML() {
+    let hobbyInfo = '';
+    hobbiesArray.forEach(hobby => {
+        hobbyInfo += (` ${hobby.name} has been a hobby for ${hobby.lengthInYearsAtHobby} years 
+        <br>  `);
+    });
+
+    return hobbyInfo;
 }
 
 
